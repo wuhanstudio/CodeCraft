16000b6433a3e730b87b13d2270dd74bea79d0c6
WuhanStudio <silenthill5@live.cn>
2016-03-30 17:25:37 +0800

    官方正式提交格式

--- future_net/future_net.cpp
+++ future_net/future_net.cpp
@@ -1,40 +1,37 @@
-#include "stdio.h"
 #include "route.h"
 #include "lib_io.h"
 #include "lib_time.h"
+#include "stdio.h"
 
 int main(int argc, char *argv[])
 {
     print_time("Begin");
-    char *graph[5000];
+    char *topo[5000];
     int edge_num;
-    char *condition;
-    int condition_num;
+    char *demand;
+    int demand_num;
 
-    char *graph_file = argv[1];
-    edge_num = read_file(graph, 5000, graph_file);
-    char *condition_file = argv[2];
-    condition_num = read_file(&condition, 1, condition_file);
-    
-    /*
-    printf("------------\n"); 
-    int i=0;
-    while(graph[i])
+    char *topo_file = argv[1];
+    edge_num = read_file(topo, 5000, topo_file);
+    if (edge_num == 0)
     {
-        printf("Graph %d: %s\n",i,graph[i]);
-	i++;
+        printf("Please input valid topo file.\n");
+        return -1;
     }
-    printf("------------\n");
-    printf("Edge:%d\n",edge_num);
-    printf("------------\n");
-    printf("Condition:%s\n",condition);
-    */
-    search_route(graph, edge_num, condition);
+    char *demand_file = argv[2];
+    demand_num = read_file(&demand, 1, demand_file);
+    if (demand_num != 1)
+    {
+        printf("Please input valid demand file.\n");
+        return -1;
+    }
+
+    search_route(topo, edge_num, demand);
 
     char *result_file = argv[3];
     write_result(result_file);
-    release_buff(graph, edge_num);
-    release_buff(&condition, condition_num);
+    release_buff(topo, edge_num);
+    release_buff(&demand, 1);
 
     print_time("End");
 
