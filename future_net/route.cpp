c40f3dbeb97994da37263d8f1a4ee7dfdbaf5066
Wu Han <silenthill5@live.cn>
2016-12-31 11:27:17 +0800

    并行配置

--- future_net/route.cpp
+++ future_net/route.cpp
@@ -1,20 +1,21 @@
-#include "route.h"
-#include "lib_record.h"
 #include <stdio.h>
 #include <string.h>
 #include <malloc.h>
 #include <stdlib.h> 
+#include "route.h"
 #include "time.h"
-
+#include "lib_record.h"
+#include <omp.h>
 int split(char dst[][20], char* str, const char* spl);
-int read_demand(char *demand,int must[],int &startnode,int &endnode);//demand是输入的condition，must[50]是必经节点数组，startnode返回起点序号，endnode返回终点的序号，函数返回必经点的点数
-int feasible_childnode(int **&A,int j,int arr[2][10],int num,int path[]);//找到第i个节点的可行子节点 ,A是二维路径权重矩阵，i是父节点序号，arr[0][10]存储子节点的序号,arr[1][10]存储对应子节点的权重，k存储可行子节点数目，path是已走过的路径，num是已走过路径的点数
-int judge(int nummust,int mustarr[50],int test);//输入必经点点数，必经点数组，待测点的序号,待测点是必经点，为true否则为false
+//demand是输入的condition，must[50]是必经节点数组，startnode返回起点序号，endnode返回终点的序号，函数返回必经点的点数
+int read_demand(char *demand,int must[],int &startnode,int &endnode);
+//找到第i个节点的可行子节点 ,A是二维路径权重矩阵，i是父节点序号，arr[0][10]存储子节点的序号,arr[1][10]存储对应子节点的权重，k存储可行子节点数目，path是已走过的路径，num是已走过路径的点数
+int feasible_childnode(int **&A,int j,int arr[2][10],int num,int path[]);
+//输入必经点点数，必经点数组，待测点的序号,待测点是必经点，为true否则为false
+int judge(int nummust,int mustarr[50],int test);
 int sec(time_t &G);//返回当前的秒数
 int time_used(time_t &H);//返回以用的时间
-double similar(int num1,int *arr1,int num2,int *arr2);//计算两数组间的相似度，num1待测数组点数,arr1待测数组,num2比较数组点数,arr2比较数组
 
-int countresult = 0;
 int **a;//边的矩阵
 int edgenum;//边的条数
 int num_must;//必经点点数
@@ -30,12 +31,11 @@ time_t T;//计时用的结构体
 
 const int compare_num=15;//每个点路径信息最大存储数，用于比较
 double rate=0.7;
-
 double x1=500,x2=2;//x1必经点数量权重，x2路径权值和的权重
 
-typedef struct str
+typedef struct Node
 {
-	struct str *next;
+	struct Node *next;
 	int point;//点序号
 	int road[600];//已经经历点的路径顺序
 	int passnum;//已经经历的点的数目
@@ -44,48 +44,38 @@ typedef struct str
 	int mustnode[50];//已经经历的必经点
 }node;
 
-
 typedef struct infostr
 {
 	int must_num[compare_num];//路径经历的必经点点数
-	//int must_node[compare_num][50];//已经经历的必经点
 	int sumpow[compare_num];//路径加总权重
 }info_node;
-
-
 info_node *node_info[600];
-
-/*
-typedef struct beststr
-{
-	int num;//已经存下的路径
-	int must_num[20];//路径经历的必经点点数
-	int must_node[20][50];//已经经历的必经点
-	int sumpow[20];//路径加总权重
-}best_node;
-*/
-
-//info_node *memory[600];
-
 
 int calculate_score(node *&A, info_node *&B)
 {
 	double C[compare_num];
 	double D = x1*A->mustnum - x2*A->pow,low=x1*50.0,high=-1,E;
-	int l;
+	int lowest;
+	const int g_ncore = omp_get_num_procs(); //获取执行核的数量
+	#pragma omp parallel for num_threads(g_ncore)
 	for(int i=0;i < compare_num; i++)
 	{
 		C[i] = x1*B->must_num[i] - x2*B->sumpow[i];
 		if(low > C[i])
 		{
-			low=C[i];
-			l=i;
+			// #pragma omp critical
+			// {
+				low=C[i];
+				lowest=i;
+			// }
 		}
 		if(high < C[i])
 		{
-			high = C[i];
-		}
-		
+			// #pragma omp critical
+			// {
+				high = C[i];
+			// }
+		}	
 	}
 	if(D<low)
 	{
@@ -101,39 +91,44 @@ int calculate_score(node *&A, info_node *&B)
 	}
 	else
 	{
-		B->must_num[l]=A->mustnum;
-		B->sumpow[l]=A->pow;
+		B->must_num[lowest]=A->mustnum;
+		B->sumpow[lowest]=A->pow;
 		return 1;
 	}
 }
 
-void creat(int pointnum,int num,int path[])//pointnum当前点的点序号，num已经经历了的点的数目，path已经经历过的点 path[0]==startpoint;
+// pointnum: 当前点的点序号
+// num     : 已经经历了的点的数目
+// path    : 已经经历过的点 
+void create(int pointnum,int num,int path[])
 {
+	printf("Total Nodes Number:%d\n",num_node);
+
+	int i;
 	int k;
 	int arr[3][10];
-	int i;
-	printf("num_node:%d\n",num_node);
+	
 	int shortest[600];
 	for(i=0;i<num_node;i++)
 	{
 		shortest[i]=12000;
 	}
-	node *c= (node *)malloc(sizeof(node)),*h;
-	h=(node *)malloc(sizeof(node));
-	node *l=(node *)malloc(sizeof(node));
-	c->mustnum=0;
-	c->pow=0;
-	c->next=NULL;
-	c->passnum=num;
-	c->point=pointnum;
+
+	node *c = (node *)malloc(sizeof(node));
+	node *h = (node *)malloc(sizeof(node));
+	node *l = (node *)malloc(sizeof(node));
+	c -> mustnum = 0;
+	c -> pow = 0;
+	c -> next = NULL;
+	c -> passnum = num;
+	c -> point = pointnum;
 	memcpy(c->road, path ,c->passnum * sizeof(int));
+
 	node *r,*m,*q;
 	for(i=0;i<num_node;i++)
 	{
-		if(i==0)
-		{
-			l->next=c;
-		}
+		if(i==0) l->next=c;
+		
 		q=l;
 		l=h;
 		h=q;
@@ -146,57 +141,32 @@ void creat(int pointnum,int num,int path[])//pointnum当前点的点序号，num
 			printf("超时!最大深度%d:",r->passnum);
 			break;
 		}
+
 		while(r)
 		{
 			q = r;
 			k = feasible_childnode(a,r->point,arr,r->passnum,r->road);
-
 			for(int j=0; j < k; j++)
 			{
 				if(arr[0][j]==end_node)
 				{
-
-                    char name[50];
-					//r = (node *)malloc(sizeof(node));
 					c = (node *)malloc(sizeof(node));
-					c->point=arr[0][j];
-					c->passnum=r->passnum+1;
-					c->pow=r->pow+arr[1][j];
+					c -> point   = arr[0][j];
+					c -> passnum = r->passnum+1;
+					c -> pow     = r->pow+arr[1][j];
 					memcpy(c->road, r->road ,r->passnum * sizeof(int));
-					c->road[r->passnum]=arr[0][j];
-                    c->mustnum = r->mustnum;
-					//memcpy(c->mustnode, r->mustnode, r->mustnum * sizeof(int));
-					int estimate= (c->mustnum==num_must) ? 1 : 0;//判断路径是否符合条件		
-					//estimate=judge(num_must,must_arr,r->passnum,r->road);
-					if(estimate==1)
+					c -> road[r->passnum]=arr[0][j];
+                    c -> mustnum = r->mustnum;
+                    //判断路径是否符合条件
+					int estimate = (c->mustnum==num_must) ? 1 : 0;		
+					if(estimate)
 					{
-						//符合条件的要存下来
-						//计算路径总权重
-						//int pathpow=calculate_pow(c->passnum, c->road);
-
 						if((bestpow==-1)||(bestpow > (c->pow)))
 						{
 							bestnum = c->passnum;
 							bestpow = c->pow;
 							memcpy(bestpath, c->road ,bestnum * sizeof(int));
-							printf("bestpow:%d path:",bestpow);
-							sprintf(name,"result_%d.txt",countresult);
-
-							FILE *fid=fopen(name,"w");
-					
-							for(int o=0;o<bestnum;o++)
-							{
-								if(bestpath[o+1])
-								{
-									fprintf(fid,"%d,%d\n",bestpath[o],bestpath[o+1]);
-								}
-
-								printf("%d,",bestpath[o]);
-							}	
-							fclose(fid);
-                            printf("\n");
-                            countresult++;
-
+							printf("bestpow:%d\n",bestpow);
 						}
 					}
 					free(c);
@@ -226,7 +196,6 @@ void creat(int pointnum,int num,int path[])//pointnum当前点的点序号，num
 				}
 				else
 				{
-
 					c = (node *)malloc(sizeof(node));
 					if(arr[2][j]==1)
 					{
@@ -239,11 +208,11 @@ void creat(int pointnum,int num,int path[])//pointnum当前点的点序号，num
 						c->mustnum = r->mustnum;
 						memcpy(c->mustnode, r->mustnode, r->mustnum * sizeof(int));
 					}
-					c->point=arr[0][j];
-					c->passnum=r->passnum+1;
-					c->pow=r->pow+arr[1][j];
+					c -> point   = arr[0][j];
+					c -> passnum = r->passnum+1;
+					c -> pow     = r->pow+arr[1][j];
 					memcpy(c->road, r->road ,r->passnum * sizeof(int));
-					c->road[r->passnum]=arr[0][j];
+					c -> road[r->passnum] = arr[0][j];
 					if(calculate_score(c, node_info[arr[0][j]])==1)
 					{
 						m->next=c;
@@ -253,7 +222,6 @@ void creat(int pointnum,int num,int path[])//pointnum当前点的点序号，num
 					{
 						free(c);
 					}
-
 				}
 			}
 			r = r->next;
@@ -263,34 +231,34 @@ void creat(int pointnum,int num,int path[])//pointnum当前点的点序号，num
 	}
 }
 
-
-//你要完成的功能总入口
 void search_route(char *graph[5000], int edge_num, char *condition)
 {
 	start_time=sec(T);
 	edgenum=edge_num;
 	a = (int **)malloc(sizeof(int *) * 5000);
-	//info_node *memory[num_node];
 	int i=0;
 	while (graph[i])
 	{
 		a[i] = (int *)malloc(sizeof(int) * 3);
 		char dst[5][20];
 		split(dst, graph[i], ",");
+		// Start
 		a[i][0] = atoi(dst[1]);
+		// End
 		a[i][1] = atoi(dst[2]);
+		// Power
 		a[i][2] = atoi(dst[3]);
 		i++;
 	}
+
+	// Total Nodes
 	num_node=a[edge_num-1][0]+1;
 	num_must = read_demand(condition,must_arr,start_node,end_node);
-	
 	for(i=0; i<num_node; i++)
 	{
 		node_info[i] = (info_node *)malloc(sizeof(info_node));
-		//node_info[i]->num = 0;
 		for(int j=0;j<compare_num;j++)
-		{
+		{	
 			node_info[i]->must_num[j]=0;
 			node_info[i]->sumpow[j]=12000;
 		}
@@ -298,17 +266,19 @@ void search_route(char *graph[5000], int edge_num, char *condition)
 	
 	int PATH[num_node];
 	PATH[0]=start_node;
-	creat(start_node,1,PATH);
+	create(start_node,1,PATH);
 	
 	if(bestpow==-1)
 	{
 		printf("NA\n");
 	}
+
 	for (int i = 0; i < bestnum; i++)
 		record_result(bestpath[i]);
 }
 
-int split(char dst[][20], char* str, const char* spl)//分解字符串函数
+//分解字符串函数
+int split(char dst[][20], char* str, const char* spl)
 {
     int n = 0;
     char *result = NULL;
@@ -326,7 +296,7 @@ int read_demand(char *demand,int must[50],int &startnode,int &endnode)
 {
 	char list[50][20];
 	char sel[3][20];
-	int num_must;//必经点点数
+	int num_must; //必经点点数
 	num_must=split(list, demand, "|");
 	split(sel,list[0],",");
 	startnode=atoi(sel[0]);
@@ -342,7 +312,14 @@ int read_demand(char *demand,int must[50],int &startnode,int &endnode)
 	return num_must;
 }
 
-int feasible_childnode(int **&A,int j,int arr[3][10],int num,int path[])//找到第i个节点的可行子节点 ,A是二维路径权重矩阵，i是父节点序号，arr[0][10]存储子节点的序号,arr[1][10]存储对应子节点的权重，k存储可行子节点数目，path是已走过的路径，num是已走过路径的点数
+//找到第j个节点的可行子节点 ,
+//A是二维路径权重矩阵，
+//j是父节点序号，
+//arr[0][10]存储子节点的序号,arr[1][10]存储对应子节点的权重，
+//k存储可行子节点数目，
+//path是已走过的路径，
+//num是已走过路径的点数
+int feasible_childnode(int **&A,int j,int arr[3][10],int num,int path[])
 {
 	int k=0;//可行子节点
 	int m;
@@ -366,15 +343,16 @@ int feasible_childnode(int **&A,int j,int arr[3][10],int num,int path[])//找到
 				}
 			}
 		}
-		if(((i + 1) >= edgenum)||(((i == A[i][0]) && (i != A[i+1][0]))))
-		{
-			break;
-		}
+		// if(((i + 1) >= edgenum)||(((i == A[i][0]) && (i != A[i+1][0]))))
+		// {
+		// 	break;
+		// }
 	}
 	return k;
 }
 
-int judge(int nummust,int mustarr[50],int test)//输入必经点点数，必经点数组，待测点的序号,待测点是必经点，为true否则为false
+//输入必经点点数，必经点数组，待测点的序号,待测点是必经点，为true否则为false
+int judge(int nummust,int mustarr[50],int test)
 {
 	for(int i=0;i < nummust;i++)
 	{
@@ -397,22 +375,4 @@ int sec(time_t &G)
 int time_used(time_t &H)
 {
 	return (sec(H)-start_time);
-}
-
-double similar(int num1,int *arr1,int num2,int *arr2)//num1待测数组点数,arr1待测数组,num2比较数组点数,arr2比较数组
-{
-	int k=0;
-	for(int i=0; i<num1; i++)
-	{
-		for(int j=0; j<num2; j++)
-		{
-			if(arr1[i]==arr2[j])
-			{
-				k++;
-				break;
-			}
-		}
-	}
-	double f = k / (1.0 * num1);
-	return f;
 }
